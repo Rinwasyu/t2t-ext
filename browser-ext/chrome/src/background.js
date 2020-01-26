0d3b275a57c4f39b7d774922dbddccca2a6f68ba
Rinwasyu <rinwakun@gmail.com>
2020-01-26 21:38:48 +0900

    add context-menu & update default dictionary

--- browser-ext/chrome/src/background.js
+++ browser-ext/chrome/src/background.js
@@ -1,3 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.sendMessage(tab.id, "Translate");
+	//chrome.tabs.sendMessage(tab.id, "Translate");
+});
+
+chrome.contextMenus.create({
+	title: "Translate to Tolerance",
+	contexts: ["all"],
+	type: "normal",
+	onclick: function(info, tab) {
+		chrome.tabs.sendMessage(tab.id, "Translate");
+	}
 });
